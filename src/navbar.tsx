c01ccd5c4c4079359ade222a9dd0d2bfcdc0dcc8
AbdulMomin <35abdulmomin@gmail.com>
2026-09-13 12:46:35 +0600

    Add project humsburger

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import logoText from './assets/logo-text.png';
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="nav-container">
+        
+    
+        <button className="menu-btn" onClick={() => setIsOpen(!isOpen)}>
+          ☰
+        </button>
 
+    
         <div className="brand">
-         <img className="logo-img" src={logoText} alt="DevStack" />
+          <img className="logo-img" src={logoText} alt="DevStack" />
           <span>DevStack</span>
         </div>
 
-        <div className="nav-links">
+      
+        <div className={`nav-links ${isOpen ? 'active' : ''}`}>
           <a href="#" className="active-link">Home</a>
           <a href="#">Technologies</a>
           <a href="#">Projects</a>
@@ -17,6 +28,7 @@ function Navbar() {
           <a href="#">Contact</a>
         </div>
 
+    
         <div className="auth-buttons">
           <button className="sign-in">Sign In</button>
           <button className="sign-up">Sign Up</button>
